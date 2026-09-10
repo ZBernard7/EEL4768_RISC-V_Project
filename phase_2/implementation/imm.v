352488c24620e4dbf1465e4c87ad7f00339cccd6
ZBernard7 <zahrabernard7@gmail.com>
2026-09-10 15:00:33 -0400

    Added Part 2

--- phase_2/implementation/imm.v
+++ phase_2/implementation/imm.v
@@ -1,26 +1,58 @@
-// The immediate generator is responsible for decoding the 32-bit
-// sign-extended immediate from the incoming instruction word. It is a purely
-// combinational block that is expected to be embedded in the instruction
-// decoder.
+`default_nettype none
+
 module imm (
-    // Input instruction word. This is used to extract the relevant immediate
-    // bits and assemble them into the final immediate.
-    input  wire [31:0] i_inst,
-    // Instruction format, determined by the instruction decoder based on the
-    // opcode. This is one-hot encoded according to the following format:
-    // [0] R-type
-    // [1] I-type
-    // [2] S-type
-    // [3] B-type
-    // [4] U-type
-    // [5] J-type
-    // Because the R-type format does not have an immediate, the output
-    // immediate can be treated as a don't-care under this case.
-    input  wire [ 5:0] i_format,
-    // Output 32-bit immediate, sign-extended from the immediate bitstring.
+    input wire [31:0] i_inst,
+    input wire [5:0]  i_format,
     output wire [31:0] o_immediate
 );
-    // Your implementation goes under here
-    // ------------------------------------
+
+    reg [31:0] immediate;
+
+    always @(*) begin
+        case (i_format)
+
+            // R-type: no immediate
+            6'b000001:
+                immediate = 32'b0;
+
+            // I-type
+            6'b000010:
+                immediate = {{20{i_inst[31]}}, i_inst[31:20]};
+
+            // S-type
+            6'b000100:
+                immediate = {{20{i_inst[31]}}, i_inst[31:25], i_inst[11:7]};
+
+            // B-type
+            6'b001000:
+                immediate = {{19{i_inst[31]}},
+                             i_inst[31],
+                             i_inst[7],
+                             i_inst[30:25],
+                             i_inst[11:8],
+                             1'b0};
+
+            // U-type
+            6'b010000:
+                immediate = {i_inst[31:12], 12'b0};
+
+            // J-type
+            6'b100000:
+                immediate = {{11{i_inst[31]}},
+                             i_inst[31],
+                             i_inst[19:12],
+                             i_inst[20],
+                             i_inst[30:21],
+                             1'b0};
+
+            default:
+                immediate = 32'b0;
+
+        endcase
+    end
+
+    assign o_immediate = immediate;
 
 endmodule
+
+`default_nettype wire
