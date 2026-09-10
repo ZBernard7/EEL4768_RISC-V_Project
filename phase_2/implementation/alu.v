e5ee4cc82d8bebfd9f1f9be3d36f28528c4ff980
ZBernard7 <zahrabernard7@gmail.com>
2026-09-10 16:41:35 -0400

    extra commments to alu

--- phase_2/implementation/alu.v
+++ phase_2/implementation/alu.v
@@ -1,16 +1,56 @@
 `default_nettype none
 
+// The arithmetic logic unit (ALU) is responsible for performing the core
+// calculations of the processor. It takes two 32-bit operands and outputs
+// a 32 bit result based on the selection operation - addition, comparison,
+// shift, or logical operation. This ALU is a purely combinational block, so
+// you should not attempt to add any registers or pipeline it.
 module alu (
+    // Major operation selection.
+    // 3'b000: addition/subtraction if `i_sub` asserted
+    // 3'b001: shift left logical
+    // 3'b010: set less than
+    // 3'b011: set less than unsigned
+    // 3'b100: exclusive or
+    // 3'b101: shift right logical/arithmetic if `i_arith` asserted
+    // 3'b110: or
+    // 3'b111: and
     input  wire [ 2:0] i_opsel,
+    // When asserted, addition operations should subtract instead.
+    // This is only used for `i_opsel == 3'b000` (addition/subtraction).
     input  wire        i_sub,
+    // When asserted, comparison operations should be treated as unsigned.
+    // This is only used for branch comparisons, as the set less than unsigned
+    // mode is already specified by `i_opsel`. For branch operations, the ALU
+    // result is not used, only the comparison results.
     input  wire        i_unsigned,
+    // When asserted, right shifts should be treated as arithmetic instead of
+    // logical. This is only used for `i_opsel == 3'b101` (shift right).
     input  wire        i_arith,
+    // First 32-bit input operand.
     input  wire [31:0] i_op1,
+    // Second 32-bit input operand.
     input  wire [31:0] i_op2,
+    // 32-bit output result. Any carry out (from addition) should be ignored.
     output wire [31:0] o_result,
+    // Equality result. This is used downstream to determine if a
+    // branch should be taken.
     output wire        o_eq,
+    // Set less than result. This is used downstream to determine if a
+    // branch should be taken.
     output wire        o_slt
 );
+    // Your implementation goes under here
+    // ------------------------------------
+
+    //Part 1 of Phase #2
+        
+    // 32-bit 2's-complement ADD/SUB w/ Carry Lookahead Adder
+
+
+    // Addition: op2_modified = i_op2,so carry[0] = 0 (same as A+B)
+    // Subtraction: op2_modified = ~i_op2,so carry[0] = 1 (same as A+1+~B)
+
     wire [31:0] op2_modified;
     assign op2_modified = i_op2 ^ {32{i_sub}};
 
@@ -21,64 +61,69 @@ module alu (
     assign propagate = i_op1 ^ op2_modified;
     wire [32:0] carry;
     assign carry[0] = i_sub;
-
-    wire [7:0] group_generate;
+// 8 groups of 4 bits,each group has its own CLA
+    wire [7:0] group_generate;//i put the 32 bits into 8 groups (so basically 4 bits at a time)
     wire [7:0] group_propagate;
-
+//bite 3:0
     assign group_propagate[0] =
         propagate[3] & propagate[2] & propagate[1] & propagate[0];
     assign group_generate[0] =
         gener[3] | (propagate[3] & gener[2]) | (propagate[3] & propagate[2] & gener[1]) |
         (propagate[3] & propagate[2] & propagate[1] & gener[0]);
-
+//bits 7:4
     assign group_propagate[1] =
         propagate[7] & propagate[6] & propagate[5] & propagate[4];
     assign group_generate[1] =
         gener[7] | (propagate[7] & gener[6]) | (propagate[7] & propagate[6] & gener[5]) |
         (propagate[7] & propagate[6] & propagate[5] & gener[4]);
-
+//bits 11:8
     assign group_propagate[2] =
         propagate[11] & propagate[10] & propagate[9] & propagate[8];
     assign group_generate[2] =
         gener[11] | (propagate[11] & gener[10]) | (propagate[11] & propagate[10] & gener[9]) |
         (propagate[11] & propagate[10] & propagate[9] & gener[8]);
-
+//bit 15:12
     assign group_propagate[3] =
         propagate[15] & propagate[14] & propagate[13] & propagate[12];
     assign group_generate[3] =
         gener[15] | (propagate[15] & gener[14]) | (propagate[15] & propagate[14] & gener[13]) |
         (propagate[15] & propagate[14] & propagate[13] & gener[12]);
-
+// 19:16
     assign group_propagate[4] =
         propagate[19] & propagate[18] & propagate[17] & propagate[16];
     assign group_generate[4] =
         gener[19] | (propagate[19] & gener[18]) | (propagate[19] & propagate[18] & gener[17]) |
         (propagate[19] & propagate[18] & propagate[17] & gener[16]);
-
+//bit 23:20
     assign group_propagate[5] =
         propagate[23] & propagate[22] & propagate[21] & propagate[20];
     assign group_generate[5] =
         gener[23] | (propagate[23] & gener[22]) | (propagate[23] & propagate[22] & gener[21]) |
         (propagate[23] & propagate[22] & propagate[21] & gener[20]);
-
+//bits: 27:24
     assign group_propagate[6] =
     propagate[27] & propagate[26] & propagate[25] & propagate[24];
     assign group_generate[6] =
         gener[27] | (propagate[27] & gener[26]) | (propagate[27] & propagate[26] & gener[25]) |
         (propagate[27] & propagate[26] & propagate[25] & gener[24]);
-
+//bits: 31:28
     assign group_propagate[7] =
         propagate[31] & propagate[30] & propagate[29] & propagate[28];
     assign group_generate[7] =
         gener[31] | (propagate[31] & gener[30]) | (propagate[31] & propagate[30] & gener[29]) |
         (propagate[31] & propagate[30] & propagate[29] & gener[28]);
 
+
+//Lookahead for the groups, the carry being assigned is going to the next group
+    //PROPAGATE MEANS PASS A CARRY THROUGH
+
     assign carry[4] =
         group_generate[0] | (group_propagate[0] & carry[0]);
+    
     assign carry[8] =
         group_generate[1] | (group_propagate[1] & group_generate[0]) |
         (group_propagate[1] & group_propagate[0] & carry[0]);
-
+//carry[8] is coming out of group 1
     assign carry[12] =
         group_generate[2] | (group_propagate[2] & group_generate[1]) |
         (group_propagate[2] & group_propagate[1] & group_generate[0]) |
